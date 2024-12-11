1c076dde94a7121165a067a3c2c0bbb1655d2a60
margotgoudard <goudard.margot@hotmail.com>
2024-12-11 11:59:59 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import './assets/styles/App.css'
 
 const App = () => {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === '/';
+
   return (
-    <Router>
+    <div>
+      {!isLoginPage && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,53 +1,9 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Auth from '../services/Auth';
-import LoginForm from '../components/LoginForm';
-import ChangePasswordForm from '../components/ChangePasswordForm';
-import '../assets/styles/App.css';
-import logo from '../assets/images/logo.png';
-import background from '../assets/images/background.png';
+import React from 'react';
+import { useSelector } from 'react-redux'; 
+import '../assets/styles/Profile.css';
 
-const Login = () => {
-  const [showComponent, setShowComponent] = useState('login');
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogin = async (email, password) => {
-    try {
-      const { user, token } = await Auth.login(email, password);
-      dispatch({ type: 'auth/login', payload: { user, token } });
-
-      if (user.last_connected) {
-        navigate('/profile');
-      } else {
-        setShowComponent('changePassword');
-      }
-    } catch (error) {
-      console.error('Erreur lors de la connexion :', error);
-      alert(error.message || 'Échec de la connexion. Vérifiez vos informations.');
-    }
-  };
-
-  const handleChangePassword = async (newPassword) => {
-    try {
-      alert('Mot de passe changé avec succès.');
-      navigate('/profile');
-    } catch (error) {
-      console.error('Erreur lors du changement de mot de passe :', error);
-      alert(error.message || 'Impossible de changer le mot de passe.');
-    }
-  };
-
-  return (
-    <div className="login-container" style={{ backgroundImage: `url(${background})` }}>
-      <div className="form-container">
-        <img src={logo} alt="Logo" className="logo" />
-        {showComponent === 'login' && <LoginForm onLogin={handleLogin} />}
-        {showComponent === 'changePassword' && <ChangePasswordForm onChangePassword={handleChangePassword} />}
-      </div>
-    </div>
-  );
+const Profile = () => {
+ 
 };
 
-export default Login;
+export default Profile;
